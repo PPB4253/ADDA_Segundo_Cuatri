cf7a8ce70b749d90107768c0db6d3d384238699c
“PPB4253” <adrjimort@alum.us.es>
2026-02-21 22:14:44 +0100

    Ejercicio1 da ERROR

--- EjerciciosMios/src/Ejemplo1/Test.java
+++ EjerciciosMios/src/Ejemplo1/Test.java
@@ -13,17 +13,17 @@ public class Test {
 		
 		// 2. Crear (o copiar) la clase de datos del problema y cargar los datos del problema.
 		
-		Datos.iniDatos("./src/Ejemplo1/ejemplo1_1.txt");
+		Datos.iniDatos("src/Ejemplo1/ejemplo1_1.txt");
 		
 		// 3. Escribir el modelo en ".lsi".
 		// En archivo .lsi
 		
 		// 4. Traducir el lsi a lo que entiende Gurobi (.lp) y ejecutar Gurobi.
 		AuxGrammar.generate(Datos.class, 
-				"./src/Ejemplo1/ejemplo1.lsi", 	// COGEMOS EL .lsi
-				"./src/Ejemplo1/ejemplo1.lp");	// GENERAMOS EL .lp
+				"src/Ejemplo1/ejemplo1.lsi", 	// COGEMOS EL .lsi
+				"src/Ejemplo1/ejemplo1.lp");	// GENERAMOS EL .lp
 		
-		var sol = GurobiLp.gurobi("./src/Ejemplo1/ejemplo1.lp");	// EJECUTAMOS EL .lp
+		var sol = GurobiLp.gurobi("src/Ejemplo1/ejemplo1.lp");	// EJECUTAMOS EL .lp
 		
 		// 5. Procesar la solución.
 		if (sol.isEmpty()) {
---
+++ EjerciciosMios/src/ejercicio1/ple/Test.java
@@ -0,0 +1,38 @@
+package ejercicio1.ple;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import us.lsi.gurobi.GurobiLp;
+import us.lsi.solve.AuxGrammar;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		// 1. Ver y entender los datos de entrada.
+		// Formalizar a papel primero
+		
+		// 2. Crear (o copiar) la clase de datos del problema y cargar los datos del problema.
+
+		Locale.setDefault(Locale.of("es", "ES"));
+		for(Integer i: List.of(1, 2, 3)) { // indique los tests a realizar
+			Datos1.iniDatos("src/ejercicio1/ple/DatosEntrada"+i+".txt");
+			// 3. Escribir el modelo en ".lsi".
+			// En archivo .lsi
+			
+			// 4. Traducir el lsi a lo que entiende Gurobi (.lp) y ejecutar Gurobi.
+			AuxGrammar.generate(Datos1.class,"src/ejercicio1/ple/Ejercicio1.lsi","src/ejercicio1/ple/Ejercicio1-"+i+".lp");
+			
+			// 5. Procesar la solución.
+			var solucion = GurobiLp.gurobi("src/ejercicio1/ple/Ejercicio1-"+i+".lp");
+			if (solucion.isPresent()) {				
+				System.out.println(solucion.get().toString((vble, valor)-> valor>0));
+			} else {
+				System.out.println("\n\n*****Modelo sin solución****");
+			}
+		}
+
+	}
+
+}
